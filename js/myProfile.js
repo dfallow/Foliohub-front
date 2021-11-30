db97e21538d2cefa90edba702119f17a4b48df72
soulyvap <soulyvap@metropolia.fi>
2021-11-30 17:42:42 +0200

    test getAll personal

--- js/myProfile.js
+++ js/myProfile.js
@@ -1,7 +1,7 @@
 'use strict';
 const url = 'http://10.114.32.29/foliohub';
 
-const div = document.querySelector('#personal');
+const div = document.querySelector('.personal');
 
 const currentUser = JSON.parse(sessionStorage.getItem('user'));
 
