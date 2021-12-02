679af4f40c46a8ccd011465a2261b40a4388750c
soulyvap <soulyvap@metropolia.fi>
2021-12-02 18:02:33 +0200

    fixing home wip

--- js/accountCreate.js
+++ js/accountCreate.js
@@ -2,37 +2,29 @@
 const url = window.GLOBAL_URL;
 
 const registerForm = document.querySelector('#register');
-const timestampInput = document.querySelector('#timestampHidden');
 
 registerForm.action = `${url}/user`
 registerForm.method = 'post'
 
 registerForm.addEventListener('submit', async (evt) => {
     evt.preventDefault();
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = ((today.getMonth()+1) < 10) ? `0${today.getMonth()+1}` : today.getMonth()+1;
-    const day = ((today.getDate()) < 10) ? `0${today.getDate()}` : today.getDate();
-    timestampInput.value = `${year}-${month}-${day}`;
+
     const data = serializeJson(registerForm);
     console.log(data);
-    const fetchOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body:
-            JSON.stringify(data),
-    };
+    // const fetchOptions = {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body:
+    //         JSON.stringify(data),
+    // };
+    //
+    // console.log('user creation response', fetchOptions.body);
+    // const response = await fetch(url + '/user', fetchOptions);
+    // const json = await response.json();
+    sessionStorage.setItem('user', JSON.stringify(data));
 
-    console.log('user creation response', fetchOptions.body);
-    const response = await fetch(url + '/user', fetchOptions);
-    const json = await response.json();
-    if(!json.user) {
-        alert(json.message);
-    } else {
-        sessionStorage.setItem('user', JSON.stringify(json.user));
-        location.href = 'accountCreateExtra.html';
-    }
-
+    console.log('data sent: ', JSON.stringify(data));
+    location.href = 'accountCreateExtra.html';
 })
--- js/accountCreateExtra.js
+++ js/accountCreateExtra.js
@@ -1,2 +1,34 @@
 'use strict';
 const url = window.GLOBAL_URL;
+
+const form = document.querySelector('#register');
+
+const dataReceived = sessionStorage.getItem('user');
+const dataJSON = JSON.parse(dataReceived);
+console.log('data received: ', dataJSON);
+
+const today = new Date();
+const year = today.getFullYear();
+const month = ((today.getMonth()+1) < 10) ? `0${today.getMonth()+1}` : today.getMonth()+1;
+const day = ((today.getDate()) < 10) ? `0${today.getDate()}` : today.getDate();
+const date = `${year}-${month}-${day}`;
+
+form.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    const data = serializeJson(form);
+    data.creationDate = date;
+    data.email = dataJSON.email;
+    data.password = dataJSON.password;
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    const response = await fetch(url + '/user', fetchOptions);
+    const json = await response.json();
+
+    location.href = 'userLogin.html';
+})
