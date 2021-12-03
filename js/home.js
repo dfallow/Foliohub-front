a2f721ac1918e2cc6b2bc03cad3ad97c52c3c1f3
soulyvap <soulyvap@metropolia.fi>
2021-12-03 19:13:10 +0200

    home page + project upload

--- js/home.js
+++ js/home.js
@@ -4,51 +4,65 @@ const url = window.GLOBAL_URL; //should be server address
 //selecting html element
 const ul = document.querySelector('#projectList');
 const profilePic = document.querySelector('#profilePic');
+const loadMore = document.querySelector('#load-more');
+let projectsExample;
 
 //creating list of projects
 const createProjectList = (projects) => {
     //clear ul element
-    for (let i = 0; i < 20; i++) {
-        if (i === 0) {
-            ul.innerHTML = '';
-        }
+    for (let i = 0; i < 3; i++) {
         projects.forEach((project) => {
             //creating li element with DOM methods
-            let figure;
-            const img = document.createElement('img');
-            console.log(project.images);
-            if (project.images) {
-                img.src = url + '/' + project.images;
-                img.alt = project.name;
 
-            } else {
-                img.src = "../images/logo.png"
-                img.alt = "No image found"
+            const src = (project.images) ? url + '/' + project.images : '';
+            const alt = (project.images) ? project.name : 'no picture';
+            const href = `../html/projectDetails.html?id=${project.id}`
 
-            }
-            figure = document.createElement('figure').appendChild(img);
+            ul.innerHTML +=
+                `<a href="${href}">
+                    <li class="project-card">
+                        <figure class="project-card-fig">
+                            <img src="${src}" alt="${alt}">
+                        </figure>
+                        <h3 class="project-card-title">${project.name}</h3>
+                    </li>
+                </a>`
 
-            const h3 = document.createElement('h3');
-            h3.innerHTML = project.name;
-
-            //view button
-            //const viewBtn = document.createElement('a');
-            //viewBtn.id = "viewBtn";
-            //viewBtn.innerHTML = 'View';
-            //viewBtn.href = `../html/projectDetails.html?id=${project.id}`
-            //viewBtn.classList.add('button');
-
-            const a = document.createElement('a');
-            a.href = `../html/projectDetails.html?id=${project.id}`;
-
-            const li = document.createElement('li');
-            li.id = "projectCard"
-
-            li.appendChild(figure);
-            li.appendChild(h3);
-            //li.appendChild(viewBtn);
-            a.appendChild(li);
-            ul.appendChild(a);
+            // let figure;
+            // const img = document.createElement('img');
+            // console.log(project.images);
+            // if (project.images) {
+            //     img.src = url + '/' + project.images;
+            //     img.alt = project.name;
+            //
+            // }
+            // img.className = "project-card-image"
+            //
+            // figure = document.createElement('figure').appendChild(img);
+            // figure.className = 'project-card-fig';
+            //
+            // const h3 = document.createElement('h3');
+            // h3.className = 'project-card-title'
+            // h3.innerHTML = project.name;
+            //
+            // //view button
+            // //const viewBtn = document.createElement('a');
+            // //viewBtn.id = "viewBtn";
+            // //viewBtn.innerHTML = 'View';
+            // //viewBtn.href = `../html/projectDetails.html?id=${project.id}`
+            // //viewBtn.classList.add('button');
+            //
+            // const a = document.createElement('a');
+            // a.href = `../html/projectDetails.html?id=${project.id}`;
+            //
+            // const li = document.createElement('li');
+            // li.id = "projectCard"
+            //
+            // li.appendChild(figure);
+            // li.appendChild(h3);
+            // //li.appendChild(viewBtn);
+            // a.appendChild(li);
+            // ul.appendChild(a);
         });
     }
 };
@@ -59,6 +73,7 @@ const getProjects = async () => {
         const response = await fetch(url + '/project');
         const projects = await response.json();
         console.log(projects)
+        projectsExample = projects;
         createProjectList(projects);
     } catch (e) {
         console.log(e.message);
@@ -70,5 +85,10 @@ profilePic.addEventListener('click', (evt) => {
     evt.preventDefault();
     location.href = 'myProfile.html';
 })
+
+loadMore.addEventListener('click', (evt => {
+    evt.preventDefault();
+    createProjectList(projectsExample);
+}))
 
 
