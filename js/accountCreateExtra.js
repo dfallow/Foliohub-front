fcfcb86b881dfb9a53490aa6acd5f10b40e02767
samhamal <samhamal@metropolia.fi>
2021-12-06 18:35:09 +0200

    stuck on multer, gave up and did projectupload

--- js/accountCreateExtra.js
+++ js/accountCreateExtra.js
@@ -3,6 +3,9 @@ const url = window.GLOBAL_URL;
 
 const form = document.querySelector('#register');
 
+form.action = url + '/user';
+form.method = 'post';
+
 const dataReceived = sessionStorage.getItem('user');
 const dataJSON = JSON.parse(dataReceived);
 console.log('data received: ', dataJSON);
@@ -15,20 +18,26 @@ const date = `${year}-${month}-${day}`;
 
 form.addEventListener('submit', async (evt) => {
     evt.preventDefault();
-    const data = serializeJson(form);
-    data.creationDate = date;
-    data.email = dataJSON.email;
-    data.password = dataJSON.password;
+    // const data = serializeJson(form);
+    const data = new FormData(form);
+    const dataObject = Object.fromEntries(data)
+    dataObject.creationDate = date;
+    dataObject.email = dataJSON.email;
+    dataObject.password = dataJSON.password;
     const fetchOptions = {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
         },
-        body: JSON.stringify(data)
+        // body: JSON.stringify(data),
+        body: data,
     }
+    for (let [key, value] of data.entries()) {
+        console.log(key, value);
+    }
+    console.log(dataObject);
+    await fetch(url + '/user', fetchOptions);
 
-    const response = await fetch(url + '/user', fetchOptions);
-    const json = await response.json();
 
-    location.href = 'userLogin.html';
+    // location.href = 'userLogin.html';
 })
