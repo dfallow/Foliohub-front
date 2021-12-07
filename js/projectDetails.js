951e7ab8facd46884dac63a872323f6ac25febd4
soulyvap <soulyvap@metropolia.fi>
2021-12-07 19:57:57 +0200

    fixing put user and continuing put project

--- js/projectDetails.js
+++ js/projectDetails.js
@@ -1,5 +1,6 @@
 'use strict';
 const url = window.GLOBAL_URL;
+console.log(url);
 
 let currentUrl = window.location.href;
 const projectId = currentUrl.split('=').pop();
@@ -7,30 +8,123 @@ const drawer = document.querySelector('#side-menu');
 
 
 //selecting html elements
-const projectDetails = document.querySelector('.projectDetails');
+const projectDetails = document.querySelector('#projectDetails');
 
 const appOverview = document.querySelector('#appOverview');
-const appOverviewTop = document.querySelector('#appOverviewTop');
-const appOverviewBottom = document.querySelector('#appOverviewBottom');
 
 const media = document.querySelector('#media');
 const comments = document.querySelector('#comments');
 const moreInfo = document.querySelector('#moreInfo');
 
-const createProjectDetails = (project) => {
+let upArrow;
+let downArrow;
 
-    //clear project details
+const createAppOverview = (project) => {
+
     projectDetails.innerHTML = '';
 
+    const src = (project.images) ? url + '/' + project.images : "../images/logo.png";
+    const alt = (project.images) ? project.name : 'no picture';
+    const img = document.createElement('img');
+    img.src = src;
+    img.alt = alt;
+
+    appOverview.innerHTML = '';
+    appOverview.innerHTML +=
+        `<div id="appOverviewTop">
+            <img src="${src}" alt="${alt}">
+            <div id="nameAuthor">
+                <h2>${project.name}</h2>
+                <p>Author</p>
+            </div>
+            <div id="card-likes">
+                <img id="arrow-up" src="../images/arrow-up.png" alt="up-arrow" onclick="upVote()"/>
+                <div id="card-like-count">9999</div>
+                <img id="arrow-down" src="../images/arrow-down.png" alt="down-arrow" onclick="downVote()"/>
+         </div>
+         </div>
+         
+         </div>
+         <div id="appOverviewBottom">
+            <div id="cardTags">
+                    <!-- TODO tags to be added here -->
+                    <p>Tags will go here</p>
+            </div>
+            <p id="shortDesc">${project.description}</p>
+         </div>`
+    projectDetails.appendChild(appOverview);
+
+    upArrow = document.querySelector('#arrow-up');
+    downArrow = document.querySelector('#arrow-down');
+
+}
+
+const createAppMedia = (project) => {
+    if (project.video) {
+        media.innerHTML +=
+            `<div id="video">
+                <iframe width="560" height="315" src="${project.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>`
+    }
+    if (project.images) {
+        const images = project.images;
+        images.forEach(
+            media.innerHTML +=
+                `<div id="images">
+                
+                </div>`
+        );
+
+    }
+
+    projectDetails.appendChild(media);
+}
+
+const createAppLongDescription = (project) => {
+
+}
+
+const createAppComments = (project) => {
+
+}
+
+const createAppMoreInfo = (project) => {
+
+}
+
+const createSimilarApps = (project) => {
+
+}
+
+const upVote = (project) => {
+    if (upArrow.style.backgroundColor === "green") {
+        upArrow.style.backgroundColor = "white";
+    } else {
+        upArrow.style.backgroundColor = "green";
+        downArrow.style.backgroundColor = "white";
+    }
+}
+
+const downVote = (project) => {
+    if (downArrow.style.backgroundColor === "red") {
+        downArrow.style.backgroundColor = "white";
+    } else {
+        upArrow.style.backgroundColor = "white";
+        downArrow.style.backgroundColor = "red";
+    }
+}
+
+/*const createProjectDetails = (project) => {
+    //clear project details
+    //projectDetails.innerHTML = '';
+
+    const src = (project.images) ? url + '/' + project.images : "../images/logo.png";
+    const alt = (project.images) ? project.name : 'no picture';
     //App logo in appOverview
     const logo = document.createElement('img');
-    if (project.images) {
-        logo.src = url + '/' + project.images;
-        logo.alt = project.name;
-    } else{
-        logo.src = "../images/logo.png";
-        logo.all = "No image found";
-    }
+
+    projectDetails.appendChild(logo);
+
     const figure = document.createElement('figure').appendChild(logo);
 
     const appName = document.createElement('h3');
@@ -67,7 +161,7 @@ const createProjectDetails = (project) => {
 
 
 
-}
+}*/
 
 //AJAX call
 const getProject = async () => {
@@ -78,7 +172,8 @@ const getProject = async () => {
         //const authorResponse = await fetch(url + '/user/' + project.author);
         //const authorId = await authorResponse.json();
         //console.log(authorId);
-        createProjectDetails(project);
+        createAppOverview(project);
+        createAppMedia(project);
     } catch (e) {
         console.log(e.message);
     }
--- js/projectUpload.js
+++ js/projectUpload.js
@@ -4,6 +4,9 @@ const url = window.GLOBAL_URL;
 const form = document.querySelector('#uploadDetails')
 const checkbox = document.querySelector('#private');
 const drawer = document.querySelector('#side-menu');
+const picturesUpload = document.querySelector('#pictures-upload');
+const uploadedPics = document.querySelector('#uploadedPictures');
+const imageUpload = document.querySelector('#image-upload');
 
 const today = new Date();
 const year = today.getFullYear();
@@ -11,19 +14,22 @@ const month = ((today.getMonth()+1) < 10) ? `0${today.getMonth()+1}` : today.get
 const day = ((today.getDate()) < 10) ? `0${today.getDate()}` : today.getDate();
 const date = `${year}-${month}-${day}`;
 
+const picturesArray = [];
+
 form.addEventListener('submit',  async (evt) => {
     evt.preventDefault();
-    // const data = new FormData(form);
-    const jsonData = serializeJson(form);
-    jsonData.date = date;
-    jsonData.private = (checkbox.checked) ? '1' : '0';
+    const data = new FormData(form);
+    data.delete('pictures');
+    data.append('date', date);
+    data.append('private', (checkbox.checked) ? '1' : '0');
+    data.append('pictures', picturesArray.toString());
+
     const fetchOptions = {
         method: 'POST',
         headers: {
             Authorization: 'Bearer ' + sessionStorage.getItem('token'),
-            'Content-type': 'application/json'
         },
-        body: JSON.stringify(jsonData),
+        body: data,
     }
 
     // for (let [key, value] of data.entries()) {
@@ -47,3 +53,42 @@ function closeDrawer() {
     drawer.style.visibility = 'hidden';
     drawer.style.width = '0';
 }
+
+
+picturesUpload.addEventListener('change', () => {
+    console.log('test');
+    const images = picturesUpload.files;
+    console.log(images);
+    const arraySelected = Array.from(images);
+    if (picturesArray.length + arraySelected.length > 6) {
+        alert('Please add 6 images in total');
+    } else {
+        arraySelected.forEach((item) => {
+            picturesArray.push(item)
+        });
+        console.log(picturesArray);
+        picturesArray.forEach(file => console.log('files', file.name));
+        updatePictures();
+
+    }
+})
+
+function updatePictures() {
+    uploadedPics.innerHTML = '';
+    picturesArray.forEach((pic, index) => {
+        console.log(pic);
+        const url = URL.createObjectURL(pic);
+        // uploadedPics.innerHTML += `<div id="uploaded-container"><img class="uploaded-pic" src="${url}"><div id="delete-image">X</div></div>`;
+        uploadedPics.innerHTML += `<img class="uploaded-pic" src="${url}" onclick="remove(${index})">`;
+    })
+}
+
+function remove(index) {
+    picturesArray.splice(index, 1);
+    updatePictures();
+}
+
+
+imageUpload.addEventListener('change', (evt) => {
+    console.log('test');
+})
