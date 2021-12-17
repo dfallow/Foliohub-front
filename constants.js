19c55de0604098c79dfb748936d6f8dfd836c490
root <root@davidfa-003.metropolia.fi>
2021-12-17 08:57:48 +0200

    fixed my profile a bit more

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@
 * Description: Setting the url required for working locally or on the virtual server.
 */
 
-const LOCAL_TESTING = true; //switch this one to false if working on the virtual server
+const LOCAL_TESTING = false; //switch this one to false if working on the virtual server
 
 window.GLOBAL_URL = (LOCAL_TESTING)
     ? 'http://localhost:3001'
